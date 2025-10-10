bc38ae72e10dcfa5c1b22c8e36e52f5dfc5d9dc7
Pondei Ebimobowei <ebi4jah15@gmail.com>
2025-10-10 11:12:11 +0100

    chore

--- frontend/src/api/wallet/wallet-api.ts
+++ frontend/src/api/wallet/wallet-api.ts
@@ -10,9 +10,9 @@ export default function WalletApi (){
 
         }catch(err:any){
             if (err.response) {
-                return { success: false, message: err.response.data.message, data: { token: "", refreshToken: "", roles: [''] } };
+                return { success: false, message: err.response.data.message, data: null };
             } else {
-                return {success: false, message: err.message, data: {token: "", refreshToken: "", roles: ['']}}
+                return {success: false, message: err.message, data: null }
             }
         }
     }
